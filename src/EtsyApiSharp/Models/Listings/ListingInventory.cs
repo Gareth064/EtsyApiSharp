Gareth064/EtsyApiSharp

commit 82c5a51a5d3edb4b6de365f1267bc11a2bf26078

Updated all models based on latest Open API Spec

src/EtsyApiSharp/Models/Listings/ListingInventory.cs
@@ -2,24 +2,34 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-    //A representation of a single listing's inventory record.
+    /// <summary>
+    /// A representation of a single listing's inventory record.
+    /// </summary>
     public class ListingInventory
     {
+        /// <summary>
+        /// A JSON array of products available in a listing, even if only one product. All field names in the JSON blobs are lowercase.
+        /// </summary>
         [JsonPropertyName("products")]
-        public List<ListingInventoryProduct> Products { get; set; }
+        public string[] Products { get; set; }
 
-
+        /// <summary>
+        /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change product prices, if any. For example, if you charge specific prices for different sized products in the same listing, then this array contains the property ID for size.
+        /// </summary>
         [JsonPropertyName("price_on_property")]
-        public List<long> PriceOnProperty { get; set; }
+        public string[] PriceOnProperty { get; set; }
 
-
+        /// <summary>
+        /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the quantity of the products, if any. For example, if you stock specific quantities of different colored products in the same listing, then this array contains the property ID for color.
+        /// </summary>
         [JsonPropertyName("quantity_on_property")]
-        public List<long> QuantityOnProperty { get; set; }
+        public string[] QuantityOnProperty { get; set; }
 
-
+        /// <summary>
+        /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the product SKU, if any. For example, if you use specific skus for different colored products in the same listing, then this array contains the property ID for color.
+        /// </summary>
         [JsonPropertyName("sku_on_property")]
-        public List<long> SkuOnProperty { get; set; }
-
+        public string[] SkuOnProperty { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingInventoryProduct.cs
@@ -2,28 +2,40 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-    //A representation of a product for a listing.
+    /// <summary>
+    /// A representation of a product for a listing.
+    /// </summary>
     public class ListingInventoryProduct
     {
+        /// <summary>
+        /// The numeric ID for a specific [product](/documentation/reference#tag/ShopListing-Product) purchased from a listing.
+        /// </summary>
         [JsonPropertyName("product_id")]
-        public int ProductId { get; set; }
+        public long ProductId { get; set; }
 
-
+        /// <summary>
+        /// The SKU string for the product
+        /// </summary>
         [JsonPropertyName("sku")]
         public string Sku { get; set; }
 
-
+        /// <summary>
+        /// When true, someone deleted this product.
+        /// </summary>
         [JsonPropertyName("is_deleted")]
         public bool IsDeleted { get; set; }
 
-
+        /// <summary>
+        /// A list of product offering entries for this product.
+        /// </summary>
         [JsonPropertyName("offerings")]
-        public List<ListingInventoryProductOffering> Offerings { get; set; }
+        public string[] Offerings { get; set; }
 
-
+        /// <summary>
+        /// A list of property value entries for this product.
+        /// </summary>
         [JsonPropertyName("property_values")]
-        public List<ListingPropertyValue> PropertyValues { get; set; }
-
+        public string[] PropertyValues { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingInventoryWithAssociations.cs
@@ -2,24 +2,40 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-    //A representation of a single listing's inventory record with associations
+    /// <summary>
+    /// A representation of a single listing's inventory record with associations
+    /// </summary>
     public class ListingInventoryWithAssociations
     {
+        /// <summary>
+        /// A JSON array of products available in a listing, even if only one product. All field names in the JSON blobs are lowercase.
+        /// </summary>
         [JsonPropertyName("products")]
-        public List<ListingInventoryProduct> Products { get; set; }
+        public string[] Products { get; set; }
 
+        /// <summary>
+        /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change product prices, if any. For example, if you charge specific prices for different sized products in the same listing, then this array contains the property ID for size.
+        /// </summary>
         [JsonPropertyName("price_on_property")]
-        public List<long> PriceOnProperty { get; set; }
+        public string[] PriceOnProperty { get; set; }
 
+        /// <summary>
+        /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the quantity of the products, if any. For example, if you stock specific quantities of different colored products in the same listing, then this array contains the property ID for color.
+        /// </summary>
         [JsonPropertyName("quantity_on_property")]
-        public List<long> QuantityOnProperty { get; set; }
+        public string[] QuantityOnProperty { get; set; }
 
+        /// <summary>
+        /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the product SKU, if any. For example, if you use specific skus for different colored products in the same listing, then this array contains the property ID for color.
+        /// </summary>
         [JsonPropertyName("sku_on_property")]
-        public List<long> SkuOnProperty { get; set; }
+        public string[] SkuOnProperty { get; set; }
 
+        /// <summary>
+        /// An enumerated string that attaches an valid association. Default value is null.
+        /// </summary>
         [JsonPropertyName("listing")]
         public ShopListing Listing { get; set; }
-
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingPropertyValue.cs
@@ -2,32 +2,46 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-    //A representation of structured data values.
+    /// <summary>
+    /// A representation of structured data values.
+    /// </summary>
     public class ListingPropertyValue
     {
+        /// <summary>
+        /// The numeric ID of the Property.
+        /// </summary>
         [JsonPropertyName("property_id")]
-        public int PropertyId { get; set; }
+        public long PropertyId { get; set; }
 
-
+        /// <summary>
+        /// The name of the Property.
+        /// </summary>
         [JsonPropertyName("property_name")]
         public string PropertyName { get; set; }
 
-
+        /// <summary>
+        /// The numeric ID of the scale (if any).
+        /// </summary>
         [JsonPropertyName("scale_id")]
-        public int ScaleId { get; set; }
+        public long? ScaleId { get; set; }
 
-
+        /// <summary>
+        /// The label used to describe the chosen scale (if any).
+        /// </summary>
         [JsonPropertyName("scale_name")]
         public string ScaleName { get; set; }
 
-
+        /// <summary>
+        /// The numeric IDs of the Property values
+        /// </summary>
         [JsonPropertyName("value_ids")]
-        public List<long> ValueIds { get; set; }
+        public string[] ValueIds { get; set; }
 
-
+        /// <summary>
+        /// The Property values
+        /// </summary>
         [JsonPropertyName("values")]
         public string[] Values { get; set; }
-
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingTranslation.cs
@@ -2,28 +2,40 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-    //Represents the translation data for a Listing.
+    /// <summary>
+    /// Represents the translation data for a Listing.
+    /// </summary>
     public class ListingTranslation
     {
+        /// <summary>
+        /// The numeric ID for the Listing.
+        /// </summary>
         [JsonPropertyName("listing_id")]
         public long ListingId { get; set; }
 
-
+        /// <summary>
+        /// The IETF language tag (e.g. 'fr') for the language of this translation.
+        /// </summary>
         [JsonPropertyName("language")]
         public string Language { get; set; }
 
-
+        /// <summary>
+        /// The title of the Listing of this Translation.
+        /// </summary>
         [JsonPropertyName("title")]
         public string Title { get; set; }
 
-
+        /// <summary>
+        /// The description of the Listing of this Translation.
+        /// </summary>
         [JsonPropertyName("description")]
         public string Description { get; set; }
 
-
+        /// <summary>
+        /// The tags of the Listing of this Translation.
+        /// </summary>
         [JsonPropertyName("tags")]
         public string[] Tags { get; set; }
-
 
     }
 }

src/EtsyApiSharp/Models/Listings/SellerTaxonomyNode.cs
@@ -2,32 +2,46 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-    //A taxonomy node in the seller taxonomy tree.
+    /// <summary>
+    /// A taxonomy node in the seller taxonomy tree.
+    /// </summary>
     public class SellerTaxonomyNode
     {
+        /// <summary>
+        /// The unique numeric ID of an Etsy taxonomy node, which is a metadata category for listings organized into the seller taxonomy hierarchy tree. For example, the \"shoes\" taxonomy node (ID: 1429, level: 1) is higher in the hierarchy than \"girls' shoes\" (ID: 1440, level: 2). The taxonomy nodes assigned to a listing support access to specific standardized product scales and properties. For example, listings assigned the taxonomy nodes \"shoes\" or \"girls' shoes\" support access to the \"EU\" shoe size scale with its associated property names and IDs for EU shoe sizes, such as property `value_id`:\"1394\", and `name`:\"38\".
+        /// </summary>
         [JsonPropertyName("id")]
         public int Id { get; set; }
 
-
+        /// <summary>
+        /// The integer depth of this taxonomy node in the seller taxonomy tree, with roots at level 0.
+        /// </summary>
         [JsonPropertyName("level")]
         public int Level { get; set; }
 
-
+        /// <summary>
+        /// The name string for this taxonomy node.
+        /// </summary>
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
-
+        /// <summary>
+        /// The numeric taxonomy ID of the parent of this node.
+        /// </summary>
         [JsonPropertyName("parent_id")]
-        public int ParentId { get; set; }
+        public long? ParentId { get; set; }
 
-
+        /// <summary>
+        /// An array of taxonomy nodes for all the direct children of this taxonomy node in the seller taxanomy tree.
+        /// </summary>
         [JsonPropertyName("children")]
-        public List<SellerTaxonomyNode> Children { get; set; }
+        public string[] Children { get; set; }
 
-
+        /// <summary>
+        /// An array of `taxonomy_id`s including this node and all of its direct parents in the seller taxonomy tree up to a root node. They are listed in order from root to leaf.
+        /// </summary>
         [JsonPropertyName("full_path_taxonomy_ids")]
-        public List<long> FullPathTaxonomyIds { get; set; }
-
+        public string[] FullPathTaxonomyIds { get; set; }
 
     }
 }

commit acc63fcf51ccf27c72292a51c9f279d811ee5646

Model fixes from bugs caused by commit 82c5a51a5d3edb4b6de365f1267bc11a2bf26078

src/EtsyApiSharp/Models/Listings/ListingInventory.cs
@@ -11,25 +11,25 @@ namespace EtsyApiSharp.Models
         /// A JSON array of products available in a listing, even if only one product. All field names in the JSON blobs are lowercase.
         /// </summary>
         [JsonPropertyName("products")]
-        public string[] Products { get; set; }
+        public List<ListingInventoryProduct> Products { get; set; }
 
         /// <summary>
         /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change product prices, if any. For example, if you charge specific prices for different sized products in the same listing, then this array contains the property ID for size.
         /// </summary>
         [JsonPropertyName("price_on_property")]
-        public string[] PriceOnProperty { get; set; }
+        public List<int> PriceOnProperty { get; set; }
 
         /// <summary>
         /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the quantity of the products, if any. For example, if you stock specific quantities of different colored products in the same listing, then this array contains the property ID for color.
         /// </summary>
         [JsonPropertyName("quantity_on_property")]
-        public string[] QuantityOnProperty { get; set; }
+        public List<int> QuantityOnProperty { get; set; }
 
         /// <summary>
         /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the product SKU, if any. For example, if you use specific skus for different colored products in the same listing, then this array contains the property ID for color.
         /// </summary>
         [JsonPropertyName("sku_on_property")]
-        public string[] SkuOnProperty { get; set; }
+        public List<int> SkuOnProperty { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingInventoryProduct.cs
@@ -29,13 +29,13 @@ namespace EtsyApiSharp.Models
         /// A list of product offering entries for this product.
         /// </summary>
         [JsonPropertyName("offerings")]
-        public string[] Offerings { get; set; }
+        public List<ListingInventoryProductOffering> Offerings { get; set; }
 
         /// <summary>
         /// A list of property value entries for this product.
         /// </summary>
         [JsonPropertyName("property_values")]
-        public string[] PropertyValues { get; set; }
+        public List<ListingPropertyValue> PropertyValues { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingInventoryWithAssociations.cs
@@ -11,25 +11,25 @@ namespace EtsyApiSharp.Models
         /// A JSON array of products available in a listing, even if only one product. All field names in the JSON blobs are lowercase.
         /// </summary>
         [JsonPropertyName("products")]
-        public string[] Products { get; set; }
+        public List<ListingInventoryProduct> Products { get; set; }
 
         /// <summary>
         /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change product prices, if any. For example, if you charge specific prices for different sized products in the same listing, then this array contains the property ID for size.
         /// </summary>
         [JsonPropertyName("price_on_property")]
-        public string[] PriceOnProperty { get; set; }
+        public List<int> PriceOnProperty { get; set; }
 
         /// <summary>
         /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the quantity of the products, if any. For example, if you stock specific quantities of different colored products in the same listing, then this array contains the property ID for color.
         /// </summary>
         [JsonPropertyName("quantity_on_property")]
-        public string[] QuantityOnProperty { get; set; }
+        public List<int> QuantityOnProperty { get; set; }
 
         /// <summary>
         /// An array of unique [listing property](/documentation/reference#operation/getListingProperties) ID integers for the properties that change the product SKU, if any. For example, if you use specific skus for different colored products in the same listing, then this array contains the property ID for color.
         /// </summary>
         [JsonPropertyName("sku_on_property")]
-        public string[] SkuOnProperty { get; set; }
+        public List<int> SkuOnProperty { get; set; }
 
         /// <summary>
         /// An enumerated string that attaches an valid association. Default value is null.

src/EtsyApiSharp/Models/Listings/ListingPropertyValue.cs
@@ -35,13 +35,13 @@ namespace EtsyApiSharp.Models
         /// The numeric IDs of the Property values
         /// </summary>
         [JsonPropertyName("value_ids")]
-        public string[] ValueIds { get; set; }
+        public List<int> ValueIds { get; set; }
 
         /// <summary>
         /// The Property values
         /// </summary>
         [JsonPropertyName("values")]
-        public string[] Values { get; set; }
+        public List<string> Values { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/ListingTranslation.cs
@@ -35,7 +35,7 @@ namespace EtsyApiSharp.Models
         /// The tags of the Listing of this Translation.
         /// </summary>
         [JsonPropertyName("tags")]
-        public string[] Tags { get; set; }
+        public List<string> Tags { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/SellerTaxonomyNode.cs
@@ -35,13 +35,13 @@ namespace EtsyApiSharp.Models
         /// An array of taxonomy nodes for all the direct children of this taxonomy node in the seller taxanomy tree.
         /// </summary>
         [JsonPropertyName("children")]
-        public string[] Children { get; set; }
+        public List<SellerTaxonomyNode> Children { get; set; }
 
         /// <summary>
         /// An array of `taxonomy_id`s including this node and all of its direct parents in the seller taxonomy tree up to a root node. They are listed in order from root to leaf.
         /// </summary>
         [JsonPropertyName("full_path_taxonomy_ids")]
-        public string[] FullPathTaxonomyIds { get; set; }
+        public List<int> FullPathTaxonomyIds { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/ShopListing.cs
@@ -149,13 +149,13 @@ namespace EtsyApiSharp.Models
         /// A list of tag strings for the listing. Valid tag strings contain only letters, numbers, whitespace characters, -, ', ™, ©, and ®. (regex: /[^\\p{L}\\p{Nd}\\p{Zs}\\-'™©®]/u) Default value is null.
         /// </summary>
         [JsonPropertyName("tags")]
-        public string[] Tags { get; set; }
+        public List<string> Tags { get; set; }
 
         /// <summary>
         /// A list of material strings for materials used in the product. Valid materials strings contain only letters, numbers, and whitespace characters. (regex: /[^\\p{L}\\p{Nd}\\p{Zs}]/u) Default value is null.
         /// </summary>
         [JsonPropertyName("materials")]
-        public string[] Materials { get; set; }
+        public List<string> Materials { get; set; }
 
         /// <summary>
         /// The numeric ID of the [shipping profile](/documentation/reference#tag/ShopListing-ShippingProfile) associated with the listing. Required when listing type=physical.
@@ -239,7 +239,7 @@ namespace EtsyApiSharp.Models
         /// An array of style strings for this listing, each of which is free-form text string such as \"Formal\", or \"Steampunk\". A Listing may have up to two styles. Valid style strings contain only letters, numbers, and whitespace characters. (regex: /[^\\p{L}\\p{Nd}\\p{Zs}]/u) Default value is null.
         /// </summary>
         [JsonPropertyName("style")]
-        public string[] Style { get; set; }
+        public List<string> Style { get; set; }
 
         /// <summary>
         /// A string describing the files attached to a digital listing.

src/EtsyApiSharp/Models/Listings/ShopListingWithAssociations.cs
@@ -149,13 +149,13 @@ namespace EtsyApiSharp.Models
         /// A list of tag strings for the listing. Valid tag strings contain only letters, numbers, whitespace characters, -, ', ™, ©, and ®. (regex: /[^\\p{L}\\p{Nd}\\p{Zs}\\-'™©®]/u) Default value is null.
         /// </summary>
         [JsonPropertyName("tags")]
-        public string[] Tags { get; set; }
+        public List<string> Tags { get; set; }
 
         /// <summary>
         /// A list of material strings for materials used in the product. Valid materials strings contain only letters, numbers, and whitespace characters. (regex: /[^\\p{L}\\p{Nd}\\p{Zs}]/u) Default value is null.
         /// </summary>
         [JsonPropertyName("materials")]
-        public string[] Materials { get; set; }
+        public List<string> Materials { get; set; }
 
         /// <summary>
         /// The numeric ID of the [shipping profile](/documentation/reference#tag/ShopListing-ShippingProfile) associated with the listing. Required when listing type=physical.
@@ -239,7 +239,7 @@ namespace EtsyApiSharp.Models
         /// An array of style strings for this listing, each of which is free-form text string such as \"Formal\", or \"Steampunk\". A Listing may have up to two styles. Valid style strings contain only letters, numbers, and whitespace characters. (regex: /[^\\p{L}\\p{Nd}\\p{Zs}]/u) Default value is null.
         /// </summary>
         [JsonPropertyName("style")]
-        public string[] Style { get; set; }
+        public List<string> Style { get; set; }
 
         /// <summary>
         /// A string describing the files attached to a digital listing.
@@ -299,19 +299,19 @@ namespace EtsyApiSharp.Models
         /// Represents a list of listing image resources, each of which contains the reference URLs and metadata for an image
         /// </summary>
         [JsonPropertyName("images")]
-        public string[] Images { get; set; }
+        public List<ListingImage> Images { get; set; }
 
         /// <summary>
         /// Represents a list of production partners for a shop.
         /// </summary>
         [JsonPropertyName("production_partners")]
-        public string[] ProductionPartners { get; set; }
+        public List<ShopProductionPartner> ProductionPartners { get; set; }
 
         /// <summary>
         /// A list of SKU strings for the listing. SKUs will only appear if the requesting user owns the shop.
         /// </summary>
         [JsonPropertyName("skus")]
-        public string[]? Skus { get; set; }
+        public List<string>? Skus { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/TaxonomyNodeProperty.cs
@@ -29,7 +29,7 @@ namespace EtsyApiSharp.Models
         /// A list of available scales.
         /// </summary>
         [JsonPropertyName("scales")]
-        public string[] Scales { get; set; }
+        public List<TaxonomyPropertyScale> Scales { get; set; }
 
         /// <summary>
         /// When true, listings assigned eligible taxonomy IDs require this property.
@@ -59,13 +59,13 @@ namespace EtsyApiSharp.Models
         /// A list of supported property value strings for this property.
         /// </summary>
         [JsonPropertyName("possible_values")]
-        public string[] PossibleValues { get; set; }
+        public List<TaxonomyPropertyValue> PossibleValues { get; set; }
 
         /// <summary>
         /// A list of property value strings automatically and always selected for the given property.
         /// </summary>
         [JsonPropertyName("selected_values")]
-        public string[] SelectedValues { get; set; }
+        public List<TaxonomyPropertyValue> SelectedValues { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Listings/TaxonomyPropertyValue.cs
@@ -29,7 +29,7 @@ namespace EtsyApiSharp.Models
         /// A list of numeric property value IDs this property value is equal to (if any).
         /// </summary>
         [JsonPropertyName("equal_to")]
-        public string[] EqualTo { get; set; }
+        public List<int> EqualTo { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/Payments/Payment.cs
@@ -149,7 +149,7 @@ namespace EtsyApiSharp.Models
         /// List of refund objects on an Etsy Payments transaction. All monetary amounts are in USD pennies unless otherwise specified.
         /// </summary>
         [JsonPropertyName("payment_adjustments")]
-        public string[] PaymentAdjustments { get; set; }
+        public List<PaymentAdjustment> PaymentAdjustments { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/ShopReceipts/ShopReceipt.cs
@@ -215,13 +215,13 @@ namespace EtsyApiSharp.Models
         /// A list of shipment statements for this receipt.
         /// </summary>
         [JsonPropertyName("shipments")]
-        public string[] Shipments { get; set; }
+        public List<ShopReceiptShipment> Shipments { get; set; }
 
         /// <summary>
         /// Array of transactions for the receipt.
         /// </summary>
         [JsonPropertyName("transactions")]
-        public string[] Transactions { get; set; }
+        public List<ShopReceiptTransaction> Transactions { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/ShopReceipts/ShopReceiptTransaction.cs
@@ -125,7 +125,7 @@ namespace EtsyApiSharp.Models
         /// Array of variations and personalizations the buyer chose.
         /// </summary>
         [JsonPropertyName("variations")]
-        public string[] Variations { get; set; }
+        public List<TransactionVariation> Variations { get; set; }
 
         /// <summary>
         /// The ID of the shipping profile selected for this listing.

src/EtsyApiSharp/Models/ShopReceipts/TransactionVariation.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace EtsyApiSharp.Models
 {
-/// <summary>
+    /// <summary>
     /// A variation chosen by the buyer during checkout.
     /// </summary>
     public class TransactionVariation

src/EtsyApiSharp/Models/ShopShippings/ShippingCarrier.cs
@@ -23,13 +23,13 @@ namespace EtsyApiSharp.Models
         /// Set of domestic mail classes of this shipping carrier.
         /// </summary>
         [JsonPropertyName("domestic_classes")]
-        public string[] DomesticClasses { get; set; }
+        public List<ShippingCarrierMailClass> DomesticClasses { get; set; }
 
         /// <summary>
         /// Set of international mail classes of this shipping carrier.
         /// </summary>
         [JsonPropertyName("international_classes")]
-        public string[] InternationalClasses { get; set; }
+        public List<ShippingCarrierMailClass> InternationalClasses { get; set; }
 
     }
 }

src/EtsyApiSharp/Models/ShopShippings/ShopShippingProfile.cs
@@ -59,13 +59,13 @@ namespace EtsyApiSharp.Models
         /// A list of [shipping profile destinations](/documentation/reference#operation/createListingShippingProfileDestination) available for this shipping profile.
         /// </summary>
         [JsonPropertyName("shipping_profile_destinations")]
-        public string[] ShippingProfileDestinations { get; set; }
+        public List<ShopShippingProfileDestination> ShippingProfileDestinations { get; set; }
 
         /// <summary>
         /// A list of [shipping profile upgrades](/documentation/reference#operation/createListingShippingProfileUpgrade) available for this shipping profile.
         /// </summary>
         [JsonPropertyName("shipping_profile_upgrades")]
-        public string[] ShippingProfileUpgrades { get; set; }
+        public List<ShopShippingProfileUpgrade> ShippingProfileUpgrades { get; set; }
 
         /// <summary>
         /// The postal code string (not necessarily a number) for the location from which the listing ships.

src/EtsyApiSharp/Models/Shops/Shop.cs
@@ -191,7 +191,7 @@ namespace EtsyApiSharp.Models
         /// A list of language strings for the shop's enrolled languages.
         /// </summary>
         [JsonPropertyName("languages")]
-        public string[] Languages { get; set; }
+        public List<string> Languages { get; set; }
 
         /// <summary>
         /// The URL string for this shop's icon image.