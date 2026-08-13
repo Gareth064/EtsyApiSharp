b8c383217c7d7fee9e209590a2f10479c18aafa1
Gareth Doherty <gareth.doherty@outlook.com>
2026-08-13 09:32:35 +0100

    Modernize Blazor test workspace

--- src/BlazorTestApp/Program.cs
+++ src/BlazorTestApp/Program.cs
@@ -1,48 +1,82 @@
 using BlazorTestApp.Data;
-using EtsyApiSharp;
 using EtsyApiSharp.Models;
-
-List<Scope> scopes = new List<Scope> { Scope.shops_r, Scope.shops_w, Scope.cart_r, Scope.listings_w, Scope.listings_r, Scope.email_r, Scope.transactions_r };
+using EtsyApiSharp.Services.Auths;
+using EtsyApiSharp.Services.ListingManagements;
+using EtsyApiSharp.Services.PaymentManagements;
+using EtsyApiSharp.Services.ReceiptManagements;
+using EtsyApiSharp.Services.ReviewManagements;
+using EtsyApiSharp.Services.ShopManagements;
+using EtsyApiSharp.Services.ShippingManagements;
 
 var builder = WebApplication.CreateBuilder(args);
+var scopes = new[] { Scope.shops_r, Scope.shops_w, Scope.cart_r, Scope.listings_w, Scope.listings_r, Scope.email_r, Scope.transactions_r };
 
-var configuration = builder.Configuration;
-var clientId = configuration["EtsyConfig:ClientId"]
-    ?? throw new InvalidOperationException("EtsyConfig:ClientId is required.");
-var sharedSecret = configuration["EtsyConfig:SharedSecret"]
-    ?? throw new InvalidOperationException("EtsyConfig:SharedSecret is required.");
-
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddSingleton<RuntimeEtsySettings>();
 builder.Services.AddHttpClient();
-builder.Services.AddEtsyAuthServiceSingleton(clientId, "https://localhost:5001/secret/callback", scopes);
-builder.Services.AddEtsyReceiptManagementServiceTransient(clientId, sharedSecret);
-builder.Services.AddEtsyPaymentManagementServiceTransient(clientId, sharedSecret);
-builder.Services.AddEtsyReviewManagementServiceTransient(clientId, sharedSecret);
-builder.Services.AddEtsyListingManagementServiceTransient(clientId, sharedSecret);
-builder.Services.AddEtsyShopManagementServiceScoped(clientId, sharedSecret);
-builder.Services.AddEtsyShippingManagementServiceTransient(clientId, sharedSecret);
 
+builder.Services.AddHttpClient(EtsyAuthService.HttpClientName);
+builder.Services.AddScoped<IEtsyAuthService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyAuthService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.RedirectUri, scopes);
+});
+
+builder.Services.AddHttpClient(EtsyReceiptManagementService.HttpClientName);
+builder.Services.AddTransient<IEtsyReceiptManagementService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyReceiptManagementService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.SharedSecretForService);
+});
+
+builder.Services.AddHttpClient(EtsyPaymentManagementService.HttpClientName);
+builder.Services.AddTransient<IEtsyPaymentManagementService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyPaymentManagementService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.SharedSecretForService);
+});
+
+builder.Services.AddHttpClient(EtsyReviewManagementService.HttpClientName);
+builder.Services.AddTransient<IEtsyReviewManagementService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyReviewManagementService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.SharedSecretForService);
+});
+
+builder.Services.AddHttpClient(EtsyListingManagementService.HttpClientName);
+builder.Services.AddTransient<IEtsyListingManagementService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyListingManagementService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.SharedSecretForService);
+});
+
+builder.Services.AddHttpClient(EtsyShopManagementService.HttpClientName);
+builder.Services.AddScoped<IEtsyShopManagementService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyShopManagementService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.SharedSecretForService);
+});
+
+builder.Services.AddHttpClient(EtsyShippingManagementService.HttpClientName);
+builder.Services.AddTransient<IEtsyShippingManagementService>(provider =>
+{
+    var settings = provider.GetRequiredService<RuntimeEtsySettings>();
+    return new EtsyShippingManagementService(provider.GetRequiredService<IHttpClientFactory>(), settings.ClientIdForService, settings.SharedSecretForService);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
